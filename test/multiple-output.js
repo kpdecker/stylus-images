475b069fe9d328d74a852f8f2ab6d3d045368a95
kpdecker <kpdecker@gmail.com>
2012-11-18 17:52:49 -0600

    Multiple output copy tests

--- test/multiple-output.js
+++ test/multiple-output.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     lib = require('./lib'),
-    stylusImages = require('../');
+    stylusImages = require('../'),
+    wrench = require('wrench');
 
 function runTest(test, options, done) {
   stylusImages(test, options)
@@ -18,7 +19,12 @@ function runTest(test, options, done) {
 }
 
 describe('multiple ouput', function() {
-  fs.mkdir('/tmp/stylus-images', parseInt('0777', 8));
+  before(function() {
+    fs.mkdir('/tmp/stylus-images', parseInt('0777', 8));
+  });
+  after(function() {
+    wrench.rmdirSyncRecursive('/tmp/stylus-images');
+  });
 
   it('should output a file for each resolution', function(done) {
     runTest(
@@ -44,8 +50,30 @@ describe('multiple ouput', function() {
       });
   });
 
-  it.skip('should copy files for each resolution', function(done) {
-    // TODO : Setup a case where one resolution does not copy and the other does
+  it('should copy files for each resolution', function(done) {
+    runTest(
+      '.test\n'
+      + '  background-image url("images/barrowLoRez.png")\n',
+
+      {images: {resolutions: [0.5, 1, 2, 3], limit: 115, copyFiles: true, outdir: '/tmp/stylus-images'}},
+
+      function(data) {
+        function expected(density) {
+          return '.test {\n'
+              + '  background-image: url("' + (density >= 2 ? lib.HI_REZ_DATA : 'images/barrowLoRez.png') + '");\n'
+              + '}\n';
+        }
+        data.should.eql({
+          '0.5': expected(0.5),
+          '1': expected(1),
+          '2': expected(2),
+          '3': expected(3)
+        });
+
+        fs.statSync('/tmp/stylus-images/images/barrowLoRez.png').isFile().should.be.true;
+
+        done();
+      });
   });
   it.skip('should merge files for each resolution', function(done) {
     // TODO : Setup a case where one resolution should merge the the other should not
